477d355610e9c5335956e455e529c727453a7d74
Hong Yue Wang <hongyuew26@gmail.com>
2020-06-23 00:33:09 -0400

    Add change, volume, MKT cap, P/E and EPS

--- src/Webscrape.java
+++ src/Webscrape.java
@@ -25,11 +25,16 @@ public class Webscrape {
         client.getOptions().setThrowExceptionOnFailingStatusCode(false);
         client.getOptions().setThrowExceptionOnScriptError(false);
 
-        final String url = "https://www.wsj.com/market-data/stocks/us/indexes";
         System.out.println("Largest companies by market cap — US Stock Market".toUpperCase());
         ArrayList<String> nameList = new ArrayList<>();
         ArrayList<String> symbolList = new ArrayList<>();
         ArrayList<String> lastList = new ArrayList<>();
+        ArrayList<String> changePercentageList = new ArrayList<>();
+        ArrayList<String> changeDollarsList = new ArrayList<>();
+        ArrayList<String> volumeList = new ArrayList<>();
+        ArrayList<String> capitalizationList = new ArrayList<>();
+        ArrayList<String> ratioList = new ArrayList<>();
+        ArrayList<String> earningsList = new ArrayList<>();
 
         try {
             HtmlPage myPage = (HtmlPage) client.getPage("https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap");
@@ -51,11 +56,28 @@ public class Webscrape {
             for (Element row : rows.subList( 1, rows.size() ) ) {
                 Element last = row.select("td").get(1);
                 lastList.add(last.ownText());
+                Element changePercentage = row.select("td").get(2);
+                changePercentageList.add(changePercentage.ownText());
+                Element changeDollars = row.select("td").get(3);
+                changeDollarsList.add(changeDollars.ownText());
+                Element volume = row.select("td").get(5);
+                volumeList.add(volume.ownText());
+                Element capitalization = row.select("td").get(6);
+                capitalizationList.add(capitalization.ownText());
+                Element ratio = row.select("td").get(7);
+                ratioList.add(ratio.ownText());
+                Element earnings = row.select("td").get(8);
+                earningsList.add(earnings.ownText());
             }
+
 
             for (int i = 0; i < nameList.size(); i++) {
 
-                System.out.println("NAME: " + nameList.get(i) + " (" + symbolList.get(i) + ")\nLAST: " + lastList.get(i) + "\n\n\n");
+                System.out.println("NAME: " + nameList.get(i) + " (" + symbolList.get(i) + ")\nLAST: " + lastList.get(i)
+                        + "\nCHANGE (%): " + changePercentageList.get(i) + "\nCHANGE ($): " + changeDollarsList.get(i)
+                        + "\nVOLUME: " + volumeList.get(i) + "\nMARKET CAPITALIZATION: " + capitalizationList.get(i)
+                        + "\nPRICE TO EARNINGS RATIO (TTM): " + ratioList.get(i) + "\nEARNINGS PER SHARE (TTM): "
+                        + earningsList.get(i) + "\n\n\n");
             }
 
             client.close();
